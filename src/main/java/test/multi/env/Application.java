16d32ad5fcce57b2cf5378cd5cd4919dda3ace91
Matthew Connors <34929018+mtthwcnnrs@users.noreply.github.com>
2022-02-15 13:16:19 -0500

    Main parts of multi env.

--- src/main/java/test/multi/env/Application.java
+++ src/main/java/test/multi/env/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         Micronaut.build(args)
+                .environments("dev","stage","test")
                 .mainClass(Application.class)
                 .start();
     }
